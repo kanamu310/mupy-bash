817680d1f50ee7b2860b09243cfbc727e75edc3a
kanamu310 <kanamu310@gmail.com>
2017-10-11 11:23:22 +0900

    take by pep8

--- play_music.py
+++ play_music.py
@@ -2,16 +2,16 @@ import struct
 import numpy as np
 from pylab import *
 
-#オリジナルモジュール
+# オリジナルモジュール
 import File_open
 import test
 import mymovie
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     freqList = File_open.conversion(File_open.openfile(sys.argv[1]))
-    datas=b""
+    datas = b""
     count = len(freqList)
-    del freqList[count-1]
+    del freqList[count - 1]
 
     freqList = [int(i) for i in freqList]
 
@@ -20,9 +20,9 @@ if __name__ == "__main__" :
         #test.play(data, 8000, 16)
         datas += data
 
-    fname=File_open.save_wave(datas)
+    fname = File_open.save_wave(datas)
     print("{0}.wavを出力".format(fname))
-    times = mymovie.main("{0}.wav".format(fname),"{0}.mp4".format(fname))
+    times = mymovie.main("{0}.wav".format(fname), "{0}.mp4".format(fname))
     print("{0}.mp4を出力".format(fname))
     mymovie.upload(fname)
     print("{0}.mp4をyoutubeにアップロード".format(fname))
--- test.py
+++ test.py
@@ -4,14 +4,17 @@ import struct
 import numpy as np
 from pylab import *
 
-def createSinWave (A, f0, fs, length):
+
+def createSinWave(A, f0, fs, length):
     """振幅A,基本周波数f0, サンプリング周波数fs,長さlength秒の正弦波を作成して返す"""
     data = []
     for n in np.arange(length * fs):
         s = A * np.sin(2 * np.pi * f0 * n / fs)
 
-        if s > 1.0: s = 1.0
-        if s < -1.0: s = -1.0
+        if s > 1.0:
+            s = 1.0
+        if s < -1.0:
+            s = -1.0
         data.append(s)
 
     data = [int(x * 32767.0) for x in data]
@@ -27,23 +30,23 @@ def play(data, fs, bit):
 
     p = pyaudio.PyAudio()
     stream = p.open(format=pyaudio.paInt16,
-            channels=1,
-            rate=int(fs),
-            output=True)
+                    channels=1,
+                    rate=int(fs),
+                    output=True)
 
     chunk = 1024
     sp = 0
-    buffer = data[sp:sp+chunk]
+    buffer = data[sp:sp + chunk]
     while buffer != b'':
         stream.write(buffer)
         sp = sp + chunk
-        buffer = data[sp:sp+chunk]
+        buffer = data[sp:sp + chunk]
 
     stream.close()
     p.terminate()
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     freqList = [262, 294, 330, 349, 392, 440, 494, 523]
     for f in freqList:
         data = createSinWave(1.0, f, 8000.0, 2.0)
